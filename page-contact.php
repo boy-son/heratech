0b56e65f6af30b971ed3a9ffbe0a0bff31112897
boy-son <59755443+boy-son@users.noreply.github.com>
2024-04-18 22:13:50 -0400

    first bit of contact page done. trying to find a good form template

--- page-contact.php
+++ page-contact.php
@@ -1,5 +1,24 @@
 <?php get_header(); ?>
+<style>
+    .contact-subtitle {
+        font-size: 2rem;
+        text-align: left;
+        padding-left: 360px;
+    }
+</style>
+<div class="wp-block-group has-global-padding is-layout-constrained wp-container-core-group-is-layout-3 wp-block-group-is-layout-constrained" style="margin-top:var(--wp--preset--spacing--60);margin-bottom:var(--wp--preset--spacing--40)">
+<h2 class="has-text-align-left alignwide wp-block-post-title has-xxxx-large-font-size">
+Contact
+</h2>
+</div>
 
-Hi!!!!!
+<!-- <div class="wp-block-group alignwide has-global-padding is-layout-constrained wp-container-core-group-is-layout-4 wp-block-group-is-layout-constrained" style="padding-right: --wp--preset--spacing--80"> -->
+<div class="wp-block-columns is-layout-flex wp-container-core-columns-is-layout-2 wp-block-columns-is-layout-flex">
+<div class="wp-block-column is-layout-flow wp-block-column-is-layout-flow" style="flex-basis:100%">
+<p class="has-large-font-size alignwide contact-subtitle">Hey, you know the Iran-Contra deal? All me, baby.</p>
+</div>
+</div>
+<!-- </div> -->
 
+</div>
 <?php get_footer(); ?>
